d0d93c763c7f2685f070e5f92cb6c30a3d75ea51
Amar Fayaz <amar@uni-bremen.de>
2020-08-11 16:19:00 -0400

    Pintype + provisions for movable pin

--- src/game_model/base_pin.py
+++ src/game_model/base_pin.py
@@ -1,6 +1,16 @@
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.button import MDRoundImageButton
 from kivy.properties import StringProperty
+from kivymd.app import MDApp
 
 class BasePin(MDRoundImageButton):
     color = StringProperty("white")
+    pin_type = StringProperty("base")
+
+    def remove_widget_from_parent(self):
+        self.parent.remove_widget(self)
+    
+    def create_playing_pin(self, size, center_pos):
+        pin_model = BasePin(color=self.color, center=center_pos, pin_type="playing")#, size=(size,size))
+        MDApp.get_running_app().game.GameBoard.add_widget(pin_model)
+        return pin_model
